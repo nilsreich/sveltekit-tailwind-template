6e0aa2736d9004627d2844f7f7302fca77c761a8
nilsreich <nilsreich@gmail.com>
2023-01-18 13:53:28 +0000

    supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
-    import "../app.css";
-  </script>
-  
-  <slot />
+<script lang="ts">
+	import { supabaseClient } from '$lib/db';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import '../app.css';
+	
+	const signout = () => {
+		supabaseClient.auth.signOut();
+	}
+
+	onMount(() => {
+		const { data: { subscription }} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
+</script>
+
+{#if $page.data.session}
+	<button on:click={signout}>Sign out</button>
+{/if}
+
+<slot />
--- src/routes/Counter.svelte
+++
@@ -1,6 +0,0 @@
-<script lang='ts'>
-	 export let count: number;
-</script>
-
-<div>Cool {count}</div>
-<button on:click={()=>count++}>Hallo - {count}</button>
